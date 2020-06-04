ac13d1619f4cc4c64b3bc6d8a23a47da69e43122
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2020-06-04 21:12:16 +0200

    Changed project structure

--- data_cleaning/acute_inflammations.py
+++ data_cleaning/acute_inflammations.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 
 # load dataset
-data_dir = os.path.join(os.path.dirname(os.getcwd()), "data")
+raw_data_dir = os.path.join(os.path.dirname(os.getcwd()), "data_raw")
 file_name = "acute_inflammations.csv"
-file_path = os.path.join(data_dir, file_name)
+file_path = os.path.join(raw_data_dir, file_name)
 dataset = pd.read_csv(file_path, sep="\t")
 
 # temperature has decimal comma in "temperature" column and is string, change it to decimal dot and to float
@@ -16,7 +16,7 @@ string_to_boolean = lambda string: True if string == "yes" else False
 dataset.iloc[:, 1:] = dataset.iloc[:, 1:].applymap(string_to_boolean)
 
 # save results
-result_file_name = "acute_inflammations_cleaned.csv"
-result_file_path = os.path.join(data_dir, result_file_name)
+cleaned_data_dir = os.path.join(os.path.dirname(os.getcwd()), "data_cleaned")
+result_file_path = os.path.join(cleaned_data_dir, file_name)
 dataset.to_csv(path_or_buf=result_file_path, index=False)
 
--- data_cleaning/cardiovascular.py
+++ data_cleaning/cardiovascular.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 # load dataset
-data_dir = os.path.join(os.path.dirname(os.getcwd()), "data")
+data_dir = os.path.join(os.path.dirname(os.getcwd()), "data_raw")
 file_name = "cardiovascular.csv"
 file_path = os.path.join(data_dir, file_name)
 dataset = pd.read_csv(file_path, sep=";")
@@ -18,6 +18,6 @@ dataset.loc[:, "age"] //= 365
 dataset[["smoke", "alco", "active", "cardio"]] = dataset[["smoke", "alco", "active", "cardio"]].astype("bool")
 
 # save results
-result_file_name = "cardiovascular_cleaned.csv"
-result_file_path = os.path.join(data_dir, result_file_name)
+cleaned_data_dir = os.path.join(os.path.dirname(os.getcwd()), "data_cleaned")
+result_file_path = os.path.join(cleaned_data_dir, file_name)
 dataset.to_csv(path_or_buf=result_file_path, index=False)
